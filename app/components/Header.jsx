97a16a5f601816dca248374b09e2772a9a141af7
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-04-08 03:35:32 +0500

    Header Component, React Server Component, use client

---
+++ app/components/Header.jsx
@@ -0,0 +1,42 @@
+'use client'
+
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const Header = () => {
+    const [client, setClient] = useState()
+  return (
+    <header className='header'>
+        <div className="container">
+            <div className="logo">
+                <Link href='/'>Next.js 13</Link>
+            </div>
+            <div className="links">
+                <Link href="/about">About</Link>
+                <Link href="/about/team">Our Team</Link>
+                <Link href="/code/repos">Code</Link>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
+
+
+
+// Advantages of React Server Side Components
+
+// _ Load Faster _ Don't have to wait for the Javascript to load 
+// _ Smaller client bundle size
+// _ SEO friendly
+// _ Access to resources the client can't access
+// _ Hide sensitive data from the client 
+// _ More secure against XSS attacks
+// _ Improve Developer Experiene
+
+// Just like with anything else, there are also disadvantages:
+
+// _ Not as interactive
+// _ No component state. We cannot use the `useState` hook.
+// _ No component lifecycle methods. We cannot use the `useEffect` hook.
