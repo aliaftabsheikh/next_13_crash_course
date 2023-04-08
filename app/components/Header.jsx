1bf548250a01a319105d5e66f536936f3e7eefe7
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-04-08 12:41:03 +0500

    Data Fetching, ReposPageOutput, Custom Laoding Page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,6 @@
-'use client'
-
 import Link from 'next/link'
-import React, { useState } from 'react'
 
 const Header = () => {
-    const [client, setClient] = useState()
   return (
     <header className='header'>
         <div className="container">
