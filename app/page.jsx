ef12be19a64028f001a7bb42b40c83bab073c349
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-04-08 19:53:46 +0500

    API route handlers, Serving course data,  Fetching course data

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,13 @@
 import Link from 'next/link'
+import Courses from './components/Courses'
 
 const HomePage = () => {
-  const routes = [
-    {
-      id: 1,
-      name: 'Home',
-      route: '/'
-    },
-    {
-      id: 2,
-      name: 'About',
-      route: '/about'
-    },
-    {
-      id: 3,
-      name: 'Team',
-      route: '/about/team'
-    },
-  ]
+  
   return (
     <div>
       <h1>Welcome to Home Page</h1>
-      {routes.map((item)=>(
-        <ul key={item.id}>
-        <li><Link href={item.route}>{item.name}</Link></li>
-      </ul>
-      ))}
+     
+     <Courses/>
       
     </div>
   )
