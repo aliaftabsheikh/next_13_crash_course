3f256f0a6393c401cc9eb909059433559d5f86ea
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-04-09 01:15:37 +0500

    Refactor server to client component

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,32 @@
-import Link from 'next/link'
+'use client';
+import { useState, useEffect } from 'react';
 import Courses from './components/Courses'
+import LoadingPage from './loading';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+  const fetchCourses = async ()=>{
+    const res = await fetch('/api/courses');
+    const data = await res.json()
+    setCourses(data);
+    setLoading(false)
+  }
+
+  fetchCourses()
+  }, [])
+
+  if(loading){
+    return <LoadingPage/>
+  }
   
+
   return (
     <div>
       <h1>Welcome to Home Page</h1>
-     
-     <Courses/>
+     <Courses courses={courses}/>
       
     </div>
   )
